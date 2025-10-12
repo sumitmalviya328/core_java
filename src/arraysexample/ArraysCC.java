dd7746f1deb069ca20e5f3c5fc6a2a5429cee612
sumitmalviya328 <sumitmalviya328@gmail.com>
2025-10-12 08:02:13 +0530

    11-10-2025

---
+++ src/arraysexample/ArraysCC.java
@@ -0,0 +1,9 @@
+package com.arraysexample;
+public class ArraysCC {
+    public static void main(String[] args) {
+      /*Classroom of 5 students - you have to store marks od these 5 students you have 2 opetion:
+        1. Create 5 variables
+        2. Use Arrays (recommended)   */
+        int [] marks = new int[5];
+    }
+}
