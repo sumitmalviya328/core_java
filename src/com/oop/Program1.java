5a0c2a88994d980aa8aaa4249d192d4dc4fecdb8
sumitmalviya328 <sumitmalviya328@gmail.com>
2025-11-06 19:19:44 +0530

    11/6/2025

--- src/com/oop/Program1.java
+++ src/com/oop/Program1.java
@@ -4,6 +4,6 @@ public class Program1 {
     int b = 20; // IV no usages
     int c = 30; // SV
     public static void main(String[] args) {
-        int a = 10; // LV
+        int a = 10; // LVProgram1
     }
 }
